208d99ff14f773ba3578fc4beff78458cae9ab84
Waqas <waqasparvaiz1@gmail.com>
2025-01-03 18:26:28 +0100

    Just commiting

---
+++ src/basis_class.py
@@ -0,0 +1,14 @@
+import numpy
+
+class Basis():
+
+    def __init__(self, dimensions):
+        self.dimensions = dimensions
+
+    @property
+    def dimension(self):
+        return self.dimension
+    
+
+    
+
