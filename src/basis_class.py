b18b3468ffebade765f0d323857c7aeee733cb3a
Waqas <waqasparvaiz1@gmail.com>
2025-01-06 15:43:17 +0100

    Second test passed

--- src/basis_class.py
+++ src/basis_class.py
@@ -1,9 +1,11 @@
 import numpy
-
 class Basis():
 
     def __init__(self, dimensions):
-        self._dimensions = dimensions
+        if dimensions % 2 != 0:
+            raise Exception("InputError: Only even positive integers allowed!")
+        else:
+            self._dimensions = dimensions
 
     @property
     def dimension(self):
@@ -12,8 +14,9 @@ class Basis():
 
     
 
-
-
+#raise Exception("Sorry, no numbers below zero")
+#if not type(x) is int:
+  #raise TypeError("Only integers are allowed")
 
     
 
