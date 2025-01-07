6fc274b68e39f848365052bba78261fbed5d21e0
Waqas <waqasparvaiz1@gmail.com>
2025-01-07 17:10:46 +0100

    Created single symmetric matrix function and passes all tests

--- src/basis_class.py
+++ src/basis_class.py
@@ -13,16 +13,25 @@ class Basis():
 
     
     def create_basis_vectors(self, index):
-        
         if index < 0: 
             raise Exception("Index can't be less than zero!")
         else:
             vector = np.zeros(self._dimensions)
             vector[index] = 1
-        
-        
         return vector
     
+    def create_single_symmetric_basis_matrix(self, first_index, second_index):
+        
+        if second_index >= first_index:
+            raise Exception("first_index must be larger than second_index!")
+        else:
+            first_index_vector = self.create_basis_vectors(first_index)
+            second_index_vector = self.create_basis_vectors(second_index)
+            matrix = 0.5*(np.outer(second_index_vector, first_index_vector) + np.outer(first_index_vector, second_index_vector))
+            return matrix
+
+    
+
 
 
 
