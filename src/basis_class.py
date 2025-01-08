2d6d190bcfe1ba9ca567498bc02eeb3e3a4c3c72
Waqas <waqasparvaiz1@gmail.com>
2025-01-08 14:32:29 +0100

    Diagonal matrix function created and passing first test

--- src/basis_class.py
+++ src/basis_class.py
@@ -40,6 +40,20 @@ class Basis():
             matrix = -0.5j*(np.outer(second_index_vector, first_index_vector) - np.outer(first_index_vector, second_index_vector))
             return matrix
 
+    def create_single_diagonal_basis_matrix(self, index):
+        
+        coeff = 1/np.sqrt(2*(index+1)*index)
+        temp_matrix = np.zeros((self.dimension, self.dimension))
+
+        for inner_index in range(index):
+            inner_index_vector = self.create_basis_vectors(inner_index)
+            temp_matrix += np.outer(inner_index_vector, inner_index_vector)
+        
+        index_vector = self.create_basis_vectors(index)
+        matrix = temp_matrix - index*np.outer(index_vector, index_vector)
+        return coeff*matrix 
+
+
     
 
 
