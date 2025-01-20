94aa03149376ac41d14ca01be508dda9dd5b2c89
Waqas <waqasparvaiz1@gmail.com>
2025-01-20 16:42:10 +0100

    Basic tests to dissipation tensor all passing

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 # README
 
-# Notes 
-
-    Python uses zero indexing, but a lot of the theory has indices starting from one, make sure it is clear to the user, that zero indexing is used in the final README file.  
+  
--- src/dissipation_tensor.py
+++ src/dissipation_tensor.py
@@ -3,12 +3,18 @@ from scipy.sparse import lil_matrix
 
 class Dissipation_Tensor():
 
-    def __init__(self, size):
+    def __init__(self, size, kossakowski_type):
         self._size = size
         self._tensor = [ 
             [lil_matrix((size, size), dtype=complex) for _ in range(size)]
         for _ in range(size)
     ]
+        if kossakowski_type.lower() == "general":
+            self._kossakowski_type =  kossakowski_type.lower()
+        elif kossakowski_type.lower() == "symmetric":
+            self._kossakowski_type =  kossakowski_type.lower()
+        else:
+            raise Exception("Kossakowski matrix type must be \"general\" or \"symmetric\"")  
     
     @property
     def size(self):
@@ -18,7 +24,12 @@ class Dissipation_Tensor():
     def tensor(self):
         return self._tensor
     
+    @property 
+    def kossakowski_type(self):
+        return self._kossakowski_type
     
+
+
 
 
 
