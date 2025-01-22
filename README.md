e50a2de962f5610f0266a5d93069a652ae76a8bd
Waqas <waqasparvaiz1@gmail.com>
2025-01-22 15:24:58 +0100

    D tensor complete with some refactoring

--- README.md
+++ README.md
@@ -6,8 +6,9 @@
 
 - Remember the indexing is from 0 to n-1 rather than in the paper which is 1 to n. 
 - Currently when building the basis can only do this for 2**7 (7 qubits systems) so in the future try to make the basis sparse too instead of regular numpy arrays.
-- For some reason, the code for building the D-tensor for n = 4 is taking 30 seconds to run however in the non-class version the nested for loops only took 4 seconds to run (not to do with the fact that I dont use a bijective map anymore). Find out where the issue is. 
-- 
+- For some reason, the code for building the D-tensor for n = 4 is taking 16 seconds to run however in the non-class version the nested for loops only took 4 seconds to run (not to do with the fact that I dont use a bijective map anymore). Find out where the issue is. 
+    -symmetric vesrion only  
+
 
 
 
--- src/dissipation_tensor.py
+++ src/dissipation_tensor.py
@@ -4,21 +4,26 @@ import time
 
 class Dissipation_Tensor():
 
-    def __init__(self, kossakowski_type, complex_structure_constants, ):
+    def __init__(self, kossakowski_type, complex_structure_constants, antisymmetric_structure_constants):
+
+        self.validate_complex_structure_constants(complex_structure_constants)
+        self._complex_structure_constants = complex_structure_constants
+        self._size = len(complex_structure_constants) 
+        self.validate_complex_structure_constants(antisymmetric_structure_constants)
+        self._antisymmetric_structure_constants = antisymmetric_structure_constants
 
         if kossakowski_type.lower() == "general":
             self._kossakowski_type =  kossakowski_type.lower()
+            self._tensor =  self.build_general_tensor()
         elif kossakowski_type.lower() == "symmetric":
             self._kossakowski_type =  kossakowski_type.lower()
+            self._tensor = self.build_symmetric_tensor()
         else:
             raise Exception("Kossakowski matrix type must be \"general\" or \"symmetric\"")  
         
         
-        self.validate_complex_structure_constants(complex_structure_constants)
-        self._complex_structure_constants = complex_structure_constants
-        self._size = len(complex_structure_constants) 
-        self._antisymmetric_structure_constants = np.real(complex_structure_constants)
-        self._tensor =  self.build_general_tensor()
+  
+        
 
 
     @property 
@@ -51,10 +56,7 @@ class Dissipation_Tensor():
         size = self._size
         complex_structure_constants = self._complex_structure_constants
         antisymmetric_structure_constants = self._antisymmetric_structure_constants
-        tensor = [ 
-                        [lil_matrix((size, size), dtype=complex) for _ in range(size)]
-                        for _ in range(size)
-                        ]
+        tensor = [[lil_matrix((size, size), dtype=complex) for _ in range(size)] for _ in range(size)]
         
         for index_1 in range(size):
             for index_2 in range(size):
@@ -74,6 +76,37 @@ class Dissipation_Tensor():
                         
                         tensor[index_1][index_2][index_3, index_4] = 0.25*value                             
         return  tensor
+    
+    def build_symmetric_tensor(self):
+        size = self._size
+        antisymmetric_structure_constants = self._antisymmetric_structure_constants
+
+        tensor = [[lil_matrix((size, size)) for _ in range(size)] for _ in range(size)]
+        
+        for index_1 in range(size):
+            # print(f"index_1 {index_1}")
+            for index_2 in range(size):
+                # print(f"index_2 = {index_2}")
+                for index_3 in range(size):
+                    # print(f"index_3 = {index_3}")
+                    for index_4 in range(size):
+                        # print(f"index_4 = {index_4}")
+                        value = 0
+                        for summed_index in range(size):
+                            # print(f"summed_index = {summed_index}")
+                            # print(antisymmetric_structure_constants[index_1][index_4, summed_index])
+                            # print(antisymmetric_structure_constants[index_2][index_3,summed_index])
+                            value += (antisymmetric_structure_constants[index_1][index_4, summed_index])*(antisymmetric_structure_constants[index_2][index_3,summed_index]) 
+                        
+                        #if value != 0:
+                            #print(f"value = {value}, j = {index_1}, k = {index_2}, l = {index_3}, m={index_4}")
+                        
+                        tensor[index_1][index_2][index_3, index_4] = 0.5*value                             
+        return  tensor
+
+
+    
+    
 
     
 
